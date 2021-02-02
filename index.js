d14c788bfc0dde4aa1859c9b8c2cb3746826310f
RikoGit <r.git@lenta.ru>
2021-02-02 08:47:24 +0300

    test

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import Vue from 'vue';
 import App from './components/App/App.vue';
-//import 'core-js/proposals/string-replace-all.js';
-import 'core-js';
-import 'regenerator-runtime/runtime.js';
+//import 'core-js';
+//import 'regenerator-runtime/runtime.js';
 
 new Vue({
     el: '#app',
