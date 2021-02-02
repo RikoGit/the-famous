e97cdfd07cd77c1f76d0461cb7b29daecf190e0b
RikoGit <r.git@lenta.ru>
2021-02-02 08:44:09 +0300

    add core-js test ie11

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import Vue from 'vue';
 import App from './components/App/App.vue';
+//import 'core-js/proposals/string-replace-all.js';
+import 'core-js';
+import 'regenerator-runtime/runtime.js';
 
 new Vue({
     el: '#app',
